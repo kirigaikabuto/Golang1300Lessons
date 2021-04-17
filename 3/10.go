cfe28ba6793cfd3b7f3291079bbb2fe432c69a5f
Yerassyl <tleugazy98@gmail.com>
2021-04-17 12:31:54 +0600

    sdsd

--- 3/10.go
+++ 3/10.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main() {
 	sumi := 5
+	fmt.Println("start cycle")
 	for i := 0; i < 5; i++ {
+		//заходим
+		fmt.Println("i=", i)
+		fmt.Println("before sumi=",sumi)
 		sumi = sumi + 1
+		fmt.Println("after sumi=",sumi)
+		fmt.Println("")
+		//выходим
 	}
+	fmt.Println("end cycle")
 	//в начале
-	//i=0; sumi = 5;
-	//i=1; sumi = 6;
-	//i=2; sumi = 7;
+	//i=0; sumi = 5; sumi = 6
+	//i=1; sumi = 6; sumi = 7
+	//i=2; sumi = 7; sumi = 8
 	//i=3; sumi = 8;
 	//i=4; sumi = 9;
 	//i=5; sumi = 10; i<5 -> false
