61f22e1618f0a57762b002f86657903fb6871637
Yerassyl <tleugazy98@gmail.com>
2021-04-18 11:43:21 +0600

    sdsd

--- 4/2.go
+++ 4/2.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var numbers [10]int
+	fmt.Println(numbers)
+	elements := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(elements)
+	array := [...]int{1, 2, 4, 5, 6, 7, 8, 9, 2, 3, 4}
+	fmt.Println(array)
 }
