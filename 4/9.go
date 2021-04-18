e536c936a6cfe27e12684c83ab58a46c645b8e8e
Yerassyl <tleugazy98@gmail.com>
2021-04-18 11:58:46 +0600

    sdsd

---
+++ 4/9.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [...]int{1, 2, 3, 4, 5, 7, 8, 12323}
+	n := len(numbers)
+	fmt.Println(numbers)
+	for i := 0; i < n; i++ {
+		if i != 2 {
+			numbers[i] = numbers[i] * 2
+		}
+
+	}
+	fmt.Println(numbers)
+}
