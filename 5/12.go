e355cb6758df8a63a679fd472659d8f2b83193fe
Yerassyl <tleugazy98@gmail.com>
2021-04-24 11:55:41 +0600

    sdsd

--- 5/12.go
+++ 5/12.go
@@ -15,7 +15,9 @@ func main() {
 		a = append(a, k)
 	}
 	fmt.Println(a)
+	//цикл для переключения на другое число по индексу
 	for i := 0; i < len(a); i++ {
+		//цикл для сравнения элементов массива
 		for j := 0; j < len(a); j++ {
 			if a[i] < a[j] {
 				a[i], a[j] = a[j], a[i]
