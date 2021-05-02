e455643becc62824a22bb50dc6929581d38c0794
Yerassyl <tleugazy98@gmail.com>
2021-05-02 11:47:00 +0600

    sdsd

--- 6/64.go
+++ 6/64.go
@@ -9,22 +9,29 @@ import (
 func main() {
 	//strconv.Itoa-> numbers to string
 	//strconv.Atoi-> string to numbers
-	n := 2500
+	//рекурсию
+	n := 2100
 	primeNumbers := []string{}
-	for i := 1; i < n; i++ {
+	for i := 2; i < n; i++ {
 		counter := 0
-		for j := 1; j <= i; j++ {
+		for j := 2; j <= i; j++ {
 			if i%j == 0 {
 				counter += 1
 			}
 		}
-		if counter == 2 {
+		if counter == 1 {
 			primeNumbers = append(primeNumbers, strconv.Itoa(i))
 		}
 	}
 	s := strings.Join(primeNumbers, "")
-	fmt.Println(len(s))
-	arr := []int{2, 5, 6, 8, 12, 1000}
+	var c int
+	arr := []int{}
+	fmt.Scan(&c)
+	for i := 0; i < c; i++ {
+		var d int
+		fmt.Scan(&d)
+		arr = append(arr, d)
+	}
 	for _, v := range arr {
 		fmt.Print(string(s[v-1]))
 	}
