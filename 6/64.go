ce120843f3bc0fe6ccac068cb579b61101b9c276
Yerassyl <tleugazy98@gmail.com>
2021-05-02 11:34:02 +0600

    sdsd

---
+++ 6/64.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//strconv.Itoa-> numbers to string
+	//strconv.Atoi-> string to numbers
+	n := 100
+	primeNumbers := []string{}
+	for i := 1; i < n; i++ {
+		counter := 0
+		for j := 1; j <= i; j++ {
+			if i%j == 0 {
+				counter += 1
+			}
+		}
+		if counter == 2 {
+			primeNumbers = append(primeNumbers, strconv.Itoa(i))
+		}
+	}
+	s := strings.Join(primeNumbers, "")
+	arr := []int{2, 5, 6, 8, 12}
+	for _, v := range arr {
+		fmt.Print(string(s[v-1]))
+	}
+}
