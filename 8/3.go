a9882b83e517d2a3f22c8691eb5fb0b773d47d08
Yerassyl <tleugazy98@gmail.com>
2021-05-16 11:53:32 +0600

    sdsd

--- 8/3.go
+++ 8/3.go
@@ -12,9 +12,18 @@ func printMaxi(arr []int) {
 	fmt.Println(maxi)
 }
 
+func printSum(arr []int) {
+	sumi := 0
+	for i := 0; i < len(arr); i++ {
+		sumi += arr[i]
+	}
+	fmt.Println(sumi)
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6}
 	arr2 := []int{3, 4, 123, 1231, 5421}
 	printMaxi(arr)
 	printMaxi(arr2)
+	printSum(arr)
 }
