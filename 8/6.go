7ee77e9d5ee8f5032c28c605367d1454693ffffe
Yerassyl <tleugazy98@gmail.com>
2021-05-16 12:24:59 +0600

    sdsd

--- 8/6.go
+++ 8/6.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -14,9 +15,30 @@ func getNewArray(n int) []int {
 	}
 	return arr
 }
+
+func getSumArr(arr []int) int {
+	sumi := 0
+	for i := 0; i < len(arr); i++ {
+		sumi += arr[i]
+	}
+	return sumi
+}
+
 //функция для посчета суммы элементов в массиве и возвращать эту сумму
 func main() {
 	a := getNewArray(10)
 	b := getNewArray(20)
-	allArr := [][]int{a, b}
+	c := getNewArray(30)
+	d := getNewArray(40)
+	allArr := [][]int{a, b, c, d}
+	maxi := 0
+	maxiIndex := 0
+	for i := 0; i < len(allArr); i++ {
+		sumi := getSumArr(allArr[i])
+		if sumi > maxi {
+			maxi = sumi
+			maxiIndex = i
+		}
+	}
+	fmt.Println(maxiIndex)
 }
