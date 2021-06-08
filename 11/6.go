eca8ecf6ded86c75a457fc3f57e3314089496311
Yerassyl <tleugazy98@gmail.com>
2021-06-08 18:07:53 +0600

    sdsd

--- 11/6.go
+++ 11/6.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -11,6 +11,56 @@ import (
 type Product struct {
 	Name  string `json:"name"`
 	Price int    `json:"price"`
+}
+
+func InsertData(collection *mongo.Collection, product Product) (*mongo.InsertOneResult, error) {
+	result, err := collection.InsertOne(context.TODO(), product)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func ListData(collection *mongo.Collection) ([]Product, error) {
+	filter := bson.D{}
+	products := []Product{}
+	cursor, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+	err = cursor.All(context.TODO(), &products)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func GetDataByName(collection *mongo.Collection, name string) (*Product, error) {
+	filter := bson.D{{"name", name}}
+	p := &Product{}
+	err := collection.FindOne(context.TODO(), filter).Decode(&p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func UpdateData(collection *mongo.Collection, name string, product Product) error {
+	filter := bson.D{{"name", name}}
+	update := bson.D{
+		{
+			"$set",
+			bson.D{
+				{"name", product.Name},
+				{"price", product.Price},
+			},
+		},
+	}
+	_, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -30,14 +80,32 @@ func main() {
 	}
 	db := client.Database(databaseName)
 	collection := db.Collection(collectionName)
-	p1 := Product{
-		Name:  "product1",
-		Price: 130,
-	}
-	result, err := collection.InsertOne(context.TODO(), p1)
+	//p1 := Product{
+	//	Name:  "132213",
+	//	Price: 2323,
+	//}
+	//_, err = InsertData(collection, p1)
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+	//products, err := ListData(collection)
+	//for i, v := range products {
+	//	fmt.Println(i, v.Name)
+	//}
+	//pr, err := GetDataByName(collection, "132213")
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+	//fmt.Println(pr)
+	err = UpdateData(collection, "132213", Product{
+		Name:  "222222",
+		Price: 2222,
+	})
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Println(result)
+
 }
