5715fcb809cfb4e51aa4e42283c439d08436a61e
Yerassyl <tleugazy98@gmail.com>
2021-06-11 17:43:41 +0600

    sdsd

--- 12/products/postgre_store.go
+++ 12/products/postgre_store.go
@@ -1,10 +1,12 @@
 package products
 
+import "github.com/kirigaikabuto/Golang1300Lessons/12/config"
+
 type postgreProductStore struct {
 	//element from postgr (CRUD)
 }
 
-func NewPostgreProductStore() (ProductStore, error) {
+func NewPostgreProductStore(config config.PostgreConfig) (ProductStore, error) {
 	//connection to postgre
 	return &postgreProductStore{}, nil
 }
