ce88c86d9673a17f37e09a7e6c89441a44663891
Yerassyl <tleugazy98@gmail.com>
2021-06-21 18:06:13 +0600

    sdsd

--- 13/1.go
+++ 13/1.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -14,6 +15,25 @@ type ErrorMessage struct {
 
 type MegaHelloResponse struct {
 	Message string `json:"message"`
+}
+
+type Product struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+type HelloRequest struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+func CreateProduct(data *HelloRequest) *Product {
+	return &Product{
+		Id:    "1",
+		Name:  data.Name,
+		Price: data.Price,
+	}
 }
 
 func MegaHelloHandler(param1, param2 string) func(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +66,30 @@ func MegaHelloHandler(param1, param2 string) func(w http.ResponseWriter, r *http
 	}
 }
 
+func HelloHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			respondJSON(w, http.StatusBadRequest, &ErrorMessage{
+				Message: err.Error(),
+				Status:  http.StatusBadRequest,
+			})
+			return
+		}
+		req := &HelloRequest{}
+		err = json.Unmarshal(data, &req)
+		if err != nil {
+			respondJSON(w, http.StatusBadRequest, &ErrorMessage{
+				Message: err.Error(),
+				Status:  http.StatusBadRequest,
+			})
+			return
+		}
+		product := CreateProduct(req)
+		respondJSON(w, 200, product)
+	}
+}
+
 func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
 	response, err := json.Marshal(payload)
 	if err != nil {
@@ -61,6 +105,7 @@ func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
 func main() {
 	r := mux.NewRouter()
 	r.Methods("GET").Path("/hello/{config}/{value}").HandlerFunc(MegaHelloHandler("config", "value"))
+	r.Methods("POST").Path("/hello").HandlerFunc(HelloHandler())
 	fmt.Println("server is running on port 8080")
 	http.ListenAndServe(":8080", r)
 }
