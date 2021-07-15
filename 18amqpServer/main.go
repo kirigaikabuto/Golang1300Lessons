66fa58d71e8ac19742265cef8942fa6bd86ed7c4
Yerassyl <tleugazy98@gmail.com>
2021-07-15 18:02:39 +0600

    sdsd

--- 18amqpServer/main.go
+++ 18amqpServer/main.go
@@ -1,1 +1,33 @@
-package _8amqpServer
+package main
+
+import (
+	"github.com/djumanoff/amqp"
+	"github.com/kirigaikabuto/Golang1300Lessons/18amqpServer/utils"
+)
+
+func main() {
+	config := amqp.Config{
+		Host: "localhost",
+		Port: 5672,
+	}
+	serverConfig := amqp.ServerConfig{
+		ResponseX: "response",
+		RequestX:  "request",
+	}
+	sess := amqp.NewSession(config)
+	err := sess.Connect()
+	if err != nil {
+		panic(err)
+		return
+	}
+	srv, err := sess.Server(serverConfig)
+	if err != nil {
+		panic(err)
+		return
+	}
+	srv.Endpoint("lesson18.GetNumber", utils.AMQPGetNumber())
+	if err := srv.Start(); err != nil {
+		panic(err)
+		return
+	}
+}
--- 18amqpServer/utils/amqp_endpoints.go
+++ 18amqpServer/utils/amqp_endpoints.go
@@ -1,1 +1,11 @@
 package utils
+
+import "github.com/djumanoff/amqp"
+
+func AMQPGetNumber() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		res := GetNumber()
+		response := &amqp.Message{Body: []byte(res)}
+		return response
+	}
+}
